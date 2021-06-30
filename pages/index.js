4ccd4e1ca6dc448e58a3cbeb9f0ecf6edc8bbf9d
Luke Hol <lukehol2000@gmail.com>
2021-06-30 21:27:35 +0200

    index: Removed comments

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,12 @@ export default function Home() {
   return (
     <div className="bg-white dark:bg-gray-900">
 			<Head>
-				{/* SEO */}
 				<title>Luke Hol</title>
 				<meta name="description">A developer from the Netherlands specialized in Typescript and .NET Software Engineering.</meta>
-				{/* Font */}
-				<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 
-				{/* Meta */}
+				<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-				{/* Favicons */}
 				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
