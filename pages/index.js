6f89ce491a0bc7b0e1075bc79a18d5e407d0f3a5
Luke Hol <lukehol2000@gmail.com>
2021-06-30 21:01:29 +0200

    Added favicon

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,22 @@ export default function Home() {
   return (
     <div className="bg-white dark:bg-gray-900">
 			<Head>
+				{/* Font */}
 				<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+
+				{/* Meta */}
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+				{/* Favicons */}
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+				<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#1d74ff" />
+				<link rel="shortcut icon" href="/favicon.ico" />
+				<meta name="msapplication-TileColor" content="#1d74ff" />
+				<meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+				<meta name="theme-color" content="#1D74FF" />
 			</Head>
 			<div className="flex flex-col items-center | min-w-screen min-h-screen | p-4">
 				<div className="mt-12 md:mt-16 relative">
